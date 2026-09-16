d442faa787ac20a7e5a72b592535096d65fdf67b
qerib-seferli <seferovqerib09@gmail.com>
2026-09-17 00:51:02 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='avtovip-v42-20260917-story-mobile';
-const CORE=['./','index.html','assets/css/app.css?v=42','assets/css/patch11.css?v=34','assets/css/ui-pro.css?v=39','assets/js/supabase.js','assets/js/international.js','assets/js/app.js?v=42','assets/js/social.js?v=31','assets/js/patch11.js?v=20','assets/js/ui-pro.js?v=24','assets/img/brand/icon-192.png'];
+const CACHE='avtovip-v43-20260917-story-safe-area';
+const CORE=['./','index.html','assets/css/app.css?v=43','assets/css/patch11.css?v=34','assets/css/ui-pro.css?v=39','assets/js/supabase.js','assets/js/international.js','assets/js/app.js?v=42','assets/js/social.js?v=31','assets/js/patch11.js?v=20','assets/js/ui-pro.js?v=24','assets/img/brand/icon-192.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
