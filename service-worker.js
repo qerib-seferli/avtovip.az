a525c0cae9bdb9236efb69ae11ae7681f450b08b
qerib-seferli <seferovqerib09@gmail.com>
2026-09-21 21:49:43 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='avtovip-v95';
+const CACHE='avtovip-v96';
 const STATIC=[
   './assets/css/app.css','./assets/css/patch11.css','./assets/css/ui-pro.css',
   './assets/js/supabase.js','./assets/js/international.js','./assets/js/app.js','./assets/js/social.js','./assets/js/patch11.js','./assets/js/ui-pro.js','./assets/js/feed-v20.js',
