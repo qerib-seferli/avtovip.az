66e67294be45682b50f3cc856158234c12fca51e
qerib-seferli <seferovqerib09@gmail.com>
2026-09-23 01:36:35 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE='avtovip-v102-verified-single';
+const CACHE='avtovip-v103-identity-frames';
 const STATIC=[
   './assets/css/app.css','./assets/css/patch11.css','./assets/css/ui-pro.css','./assets/css/profile-final.css',
   './assets/js/supabase.js','./assets/js/international.js','./assets/js/app.js','./assets/js/social.js','./assets/js/patch11.js','./assets/js/ui-pro.js','./assets/js/feed-v20.js','./assets/js/profile-final.js',
-  './assets/img/brand/logo.png','./assets/img/brand/icon-192.png','./assets/img/brand/icon-512.png','./site.webmanifest'
+  './assets/img/brand/logo.png','./assets/img/brand/icon-192.png','./assets/img/brand/icon-512.png','./assets/img/identity/vip-frame.svg','./assets/img/identity/premium-frame.svg','./site.webmanifest'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
